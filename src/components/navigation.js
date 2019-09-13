686f4457c4c252c6e7848f4bfd2afd159586350d
Mateusz <mrpeciak@gmail.com>
2019-09-13 06:46:56 +0200

    add navigation component

---
+++ src/components/navigation.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Link } from "gatsby"
+import './../main.scss'
+
+const Navigation = () => {
+     return (
+          <React.Fragment>
+               <nav className="nav-bar">
+                    <Link to="/" className="navigation__item " activeClassName="active ">O mnie</Link>
+                    <Link to="/muzyka" className="navigation__item "  activeClassName="active ">Muzyka</Link>
+                    <Link to="/aktualnosci" className="navigation__item " activeClassName="active ">Aktualności</Link>
+                    <Link to="/koncerty" className="navigation__item " activeClassName="active ">Koncerty</Link>
+                    <Link to="/galeria-zdjec" className="navigation__item " activeClassName="active ">Galeria zdjęć</Link>
+                    <Link to="/kontakt" className="navigation__item " activeClassName="active ">Kontakt</Link>
+               </nav>
+               <nav className="navigation-aside">
+                    <Link to="/" className="navigation-aside__item" activeClassName="active-aside"></Link>
+                    <Link to="/muzyka" className="navigation-aside__item" activeClassName="active-aside"></Link>
+                    <Link to="/aktualnosci" className="navigation-aside__item" activeClassName="active-aside"></Link>
+                    <Link to="/koncerty" className="navigation-aside__item" activeClassName="active-aside"></Link>
+                    <Link to="/galeria-zdjec" className="navigation-aside__item" activeClassName="active-aside"></Link>
+                    <Link to="/kontakt" className="navigation-aside__item" activeClassName="active-aside"></Link>
+               </nav>
+          </React.Fragment>
+     );
+};
+
+export default Navigation;
