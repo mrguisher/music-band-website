975e2609da8e303afaff269026dae9f98ee1caf2
Mateusz <mrpeciak@gmail.com>
2019-09-17 19:59:13 +0200

    add band-members section

---
+++ src/pages/zespol.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import './../main.scss'
+import MainWrapper from '../components/mainWrapper';
+
+
+const Zespol = () => {
+     return (
+          <MainWrapper navColor="white">
+          <div className="main band-members">
+               <p>contact</p>
+               
+          </div>
+          </MainWrapper>
+     );
+};
+
+export default Zespol;
