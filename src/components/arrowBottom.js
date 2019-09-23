b5821b11a37e904442a849bbeabea1c1a1b9ae1c
Mateusz <mrpeciak@gmail.com>
2019-09-23 20:12:54 +0200

    add arrow component

---
+++ src/components/arrowBottom.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './../main.scss'
+
+const ArrowBottom = () => (
+     <React.Fragment>
+          <div className="arrow-bottom"></div>
+     </React.Fragment>
+   )
+
+export default ArrowBottom;
